075c837b4f6a410d8d7722a6ebb7675035a01ae4
Roberto <rc@ie.gt>
2024-08-07 00:03:54 -0600

    agregando sitemap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -331,4 +331,22 @@
 </section>
     <script src="//unpkg.com/alpinejs" defer></script>
 </body>
+
+    <script>
+      (function(d,t) {
+        var BASE_URL="https://chat.hacelogeek.com";
+        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=BASE_URL+"/packs/js/sdk.js";
+        g.defer = true;
+        g.async = true;
+        s.parentNode.insertBefore(g,s);
+        g.onload=function(){
+          window.chatwootSDK.run({
+            websiteToken: 'YLEntfiGDotgeimtXqLaV5xR',
+            baseUrl: BASE_URL
+          })
+        }
+      })(document,"script");
+    </script>
+    
 </html>
