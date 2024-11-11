db56b15106ec86b2b4f6472726b7a9b190cbeab2
Roberto <rc@ie.gt>
2024-11-10 19:46:29 -0600

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <section class="text-gray-700 bg-white body-font">
     <div class="container flex flex-col items-center px-8 py-8 mx-auto max-w-7xl sm:flex-row">
         <a href="#_" class="text-xl font-black leading-none text-gray-900 select-none logo">#HaceloGeek<span class="text-indigo-600">.</span></a>
-        <p class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">Derechos Reservados GeekMinds © 2024</p>
+        <p class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0">Derechos Reservados 2024 © Geek Innovations LLC</p>
         <span class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start">
             <a href="https://www.facebook.com/hacelogeek" class="text-gray-400 hover:text-gray-500">
                 <span class="sr-only">Facebook</span>
