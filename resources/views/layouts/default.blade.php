5641b278bebb51e53301647687cfc732bb9322e1
Roberto <rc@ie.gt>
2025-02-21 00:37:19 -0600

    updating chat!

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -11,22 +11,8 @@
     @include('includes.footer')
     <script src="//unpkg.com/alpinejs" defer></script>
 </body>
-
-    <script>
-      (function(d,t) {
-        var BASE_URL="https://chat.hacelogeek.com";
-        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-        g.src=BASE_URL+"/packs/js/sdk.js";
-        g.defer = true;
-        g.async = true;
-        s.parentNode.insertBefore(g,s);
-        g.onload=function(){
-          window.chatwootSDK.run({
-            websiteToken: 'YLEntfiGDotgeimtXqLaV5xR',
-            baseUrl: BASE_URL
-          })
-        }
-      })(document,"script");
-    </script>
-    
+</script>
+    <!-- Start of HubSpot Embed Code -->
+    <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/49375184.js"></script>
+  <!-- End of HubSpot Embed Code -->    
 </html>
